4355aa81fbe104b8a30a4c3c2b03484af937dec5
Vinod <vinodmurugan12@gmail.com>
2025-02-04 08:39:21 +0530

    route commit

---
+++ user-management-frontend/src/pages/admin.tsx
@@ -0,0 +1,12 @@
+// pages/admin.tsx
+import AdminRoute from "@/components/AdminRoute";
+
+const AdminDashboard = () => {
+  return (
+    <AdminRoute>
+      <h1>Admin Dashboard</h1>
+    </AdminRoute>
+  );
+};
+
+export default AdminDashboard;
